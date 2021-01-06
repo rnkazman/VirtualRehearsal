5f8d5b5d8c71221c2e607a978612a3665d93afe4
Casey Chock <casey.chock@gmail.com>
2021-01-05 16:28:25 -1000

    Update dynu update url to include hostname

--- scripts/dynu.sh
+++ scripts/dynu.sh
@@ -1,1 +1,1 @@
-echo url="https://api.dynu.com/nic/update?username=echang&password=27c13f7329a37645f40190dc9c5be4d1" | curl -k -o /var/log/dynu.log -K - > /dev/null 2>&1
+echo url="https://api.dynu.com/nic/update?hostname=$(hostname)&username=echang&password=27c13f7329a37645f40190dc9c5be4d1" | curl -k -o /var/log/dynu.log -K - >/dev/null 2>&1
